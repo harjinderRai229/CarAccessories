d92a3d15f7d53e92890e36c52c2b807c759137eb
raisingh229 <raisingh229@gmail.com>
2023-06-10 12:03:07 +0530

    w

---
+++ .history/src/screens/test_20230610112114.js
@@ -0,0 +1,22 @@
+import React, {useContext} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {AuthContext} from '../AuthContext';
+
+const Test = () => {
+  const {user} = useContext(AuthContext);
+  // console.log(accessToken);
+  return (
+    <View style={{backgroundColor: '#000'}}>
+      <Text style={{color: '#fff'}}>
+        Name : {user.fname} {user.lname}
+      </Text>
+      <Text style={{color: '#fff'}}>
+        Mobile Number: {user.mobile_num} {user.accessToken}
+      </Text>
+    </View>
+  );
+};
+
+export default Test;
+
+const styles = StyleSheet.create({});
